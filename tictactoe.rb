0d4df85b88b31846603c38a14c5f047d1dde2833
Vaughn Antonyan <vaughnanton@gmail.com>
2017-06-25 23:04:20 -0700

    BrSt 001

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,1 +1,18 @@
 #initialize tictactoe.rb
+
+=begin
+class O
+
+class X
+
+#methods
+def move
+  if occupied, can't move there
+
+def count
+  if 3 in a row or diagonal, win and reset all
+
+instance variable=scope confined to whatever
+  object self refers to
+
+=end
